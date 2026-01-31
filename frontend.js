ba42b78396370dda0a6f5629f69bc1fe1a60fcab
viveksri1985 <m25ai2137@iitj.ac.in>
2026-01-31 14:56:17 +0530

    vcc

--- frontend.js
+++ frontend.js
@@ -1,1 +1,22 @@
-abc.txt
+const express = require('express');
+const axios = require('axios');
+const app = express();
+const PORT = 3000;
+const DB_Server_IP = '10.0.2.4';
+
+app.get('/',async (req, res) => {
+try{
+const response = await axios.get(⁠ http://${DB_Server_IP}:5000/status ⁠);
+res.send(⁠ Hey Vivek! This is Frontend calling Backend: Got response:-> ${response.data.message} ⁠);
+}catch(error) {
+res.status(500).send({
+error: "connection failed",
+message:error.message,
+details:error.code
+});
+}
+});
+
+app.listen(PORT, () => {
+console.log(⁠ Frontend is running on http://localhost/${PORT} ⁠);
+});
