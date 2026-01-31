9b1f1e3bc75319def0686e40a10e9c8ff0b02787
viveksri1985 <m25ai2137@iitj.ac.in>
2026-01-31 16:03:27 +0530

    vcc

--- backend.js
+++ backend.js
@@ -5,6 +5,6 @@ app.get('/status', (req, res) => {
 res.json({message: "Hello Vivek! This is Backend Server (Vivek-VM2). Your request has been processed and returned successfuly"});
 });
 
-app.listen(3000, '0.0.0.0', ()=> {
+app.listen(5000, '0.0.0.0', ()=> {
 console.log("Backend File is ready to process on port 5000");
 });
