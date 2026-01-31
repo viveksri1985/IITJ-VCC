63ebc3ffe87237c42af0ee25a460e21dcea0e700
viveksri1985 <m25ai2137@iitj.ac.in>
2026-01-31 15:03:35 +0530

    vcc

--- frontend.js
+++ frontend.js
@@ -2,7 +2,7 @@ const express = require('express');
 const axios = require('axios');
 const app = express();
 const PORT = 3000;
-const DB_Server_IP = '10.0.2.4';
+const DB_Server_IP = '10.0.2.5';
 
 app.get('/',async (req, res) => {
 try{
